5cd690f9a0d60ce757ae2b5b3c26d61d480b9ba1
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2024-09-30 11:58:51 +0100

    Grid initialised

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const gridContainer = document.querySelector("#grid-container");
-const squareDiv = document.createElement("div");
-squareDiv.setAttribute("id", "square-div");
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class", "square-div");
     gridContainer.appendChild(squareDiv);
   }
 }
