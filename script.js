22cef81fa04791efd0789203e981cb43dbf8e7f4
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2024-09-30 13:15:37 +0100

    Functions successfully implemented

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
 const gridContainer = document.querySelector("#grid-container");
 const btn = document.querySelector("button");
 
-let N = 16;
-for (let i = 0; i < N * N; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.setAttribute("class", "square-div");
-  gridContainer.appendChild(squareDiv);
-}
-
-let squareDivs = document.querySelectorAll(".square-div");
-
-let isMouseDown = false;
-
-document.addEventListener("mousedown", function (event) {
-  if (event.button === 0) {
-    // Left mouse button
-    isMouseDown = true;
-  }
-});
-
-document.addEventListener("mouseup", function () {
-  isMouseDown = false;
-});
-
-squareDivs.forEach((squareDiv) => {
-  squareDiv.addEventListener("mousedown", function () {
-    squareDiv.style.backgroundColor = "black";
-  });
-  squareDiv.addEventListener("mouseenter", function () {
-    if (isMouseDown) {
-      squareDiv.style.backgroundColor = "black";
-    }
-  });
-});
-
-btn.addEventListener("click", function (e) {
-  let N = Number(prompt("How many squares? "));
-  let squareDivs = document.querySelectorAll(".square-div");
-  squareDivs.forEach((squareDiv) => {
-    squareDiv.remove();
-  });
+function createGrid(N) {
+  gridContainer.innerHTML = "";
   for (let i = 0; i < N * N; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.setAttribute("class", "square-div");
     squareDiv.setAttribute("style", `flex: 0 0 calc(100% / ${N});`);
     gridContainer.appendChild(squareDiv);
   }
-  squareDivs = document.querySelectorAll(".square-div");
+}
+
+function addSketchListeners() {
+  let squareDivs = document.querySelectorAll(".square-div");
+  let isMouseDown = false;
+
+  document.addEventListener("mousedown", function (event) {
+    if (event.button === 0) {
+      // Left mouse button
+      isMouseDown = true;
+    }
+  });
+
+  document.addEventListener("mouseup", function () {
+    isMouseDown = false;
+  });
+
   squareDivs.forEach((squareDiv) => {
     squareDiv.addEventListener("mousedown", function () {
       squareDiv.style.backgroundColor = "black";
@@ -57,4 +36,14 @@ btn.addEventListener("click", function (e) {
       }
     });
   });
+}
+
+createGrid(16);
+addSketchListeners();
+
+btn.addEventListener("click", function (e) {
+  let N = Number(prompt("How many squares? "));
+  while (N > 100) N = Number(prompt("Please enter a value between 1 and 100 "));
+  createGrid(N);
+  addSketchListeners();
 });
