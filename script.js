fe3121801e1b0ef17c382c452c4ff0cfaa8d6fec
alexanderjuxoncobb <alexanderjuxoncobb@gmail.com>
2024-09-30 12:56:17 +0100

    Completed but code has repetition. Creating a branch to add functions and reduce redundancies

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 const gridContainer = document.querySelector("#grid-container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
+let N = 16;
+for (let i = 0; i < N * N; i++) {
+  const squareDiv = document.createElement("div");
+  squareDiv.setAttribute("class", "square-div");
+  gridContainer.appendChild(squareDiv);
+}
+
+let squareDivs = document.querySelectorAll(".square-div");
+
+let isMouseDown = false;
+
+document.addEventListener("mousedown", function (event) {
+  if (event.button === 0) {
+    // Left mouse button
+    isMouseDown = true;
+  }
+});
+
+document.addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
+
+squareDivs.forEach((squareDiv) => {
+  squareDiv.addEventListener("mousedown", function () {
+    squareDiv.style.backgroundColor = "black";
+  });
+  squareDiv.addEventListener("mouseenter", function () {
+    if (isMouseDown) {
+      squareDiv.style.backgroundColor = "black";
+    }
+  });
+});
+
+btn.addEventListener("click", function (e) {
+  let N = Number(prompt("How many squares? "));
+  let squareDivs = document.querySelectorAll(".square-div");
+  squareDivs.forEach((squareDiv) => {
+    squareDiv.remove();
+  });
+  for (let i = 0; i < N * N; i++) {
     const squareDiv = document.createElement("div");
     squareDiv.setAttribute("class", "square-div");
+    squareDiv.setAttribute("style", `flex: 0 0 calc(100% / ${N});`);
     gridContainer.appendChild(squareDiv);
   }
-}
+  squareDivs = document.querySelectorAll(".square-div");
+  squareDivs.forEach((squareDiv) => {
+    squareDiv.addEventListener("mousedown", function () {
+      squareDiv.style.backgroundColor = "black";
+    });
+    squareDiv.addEventListener("mouseenter", function () {
+      if (isMouseDown) {
+        squareDiv.style.backgroundColor = "black";
+      }
+    });
+  });
+});
